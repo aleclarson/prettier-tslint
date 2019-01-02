992ec66388eb8a54f61b135a35c0e9b997b3a319
aleclarson <alec.stanford.larson@gmail.com>
2019-01-01 21:32:17 -0500

    fix: revert any fixes if prettier or tslint fails

--- src/fix.js
+++ src/fix.js
@@ -1,3 +1,4 @@
+import { readFileSync, writeFileSync } from "fs";
 import runTsLint from "./run-tslint";
 import runPrettier from "./run-prettier";
 import createIgnorer from "./create-ignorer";
@@ -6,9 +7,16 @@ const fix = (filePath, isIgnored = createIgnorer()) => {
   if (isIgnored(filePath)) {
     return null;
   }
+
+  const oldContent = readFileSync(filePath, "utf8");
   const prettierCheck = runPrettier(filePath, true);
   const tslintCheck = runTsLint(filePath, true);
-  return prettierCheck && tslintCheck;
+  if (prettierCheck && tslintCheck) {
+    return true;
+  }
+
+  writeFileSync(filePath, oldContent);
+  return false;
 };
 
 export default fix;
